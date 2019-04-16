b9290e4422d3fffcd484540ba515e3b0f7a841e6
tomas.jakoubek <tomas.jakoubek@artin.cz>
2019-04-16 23:11:20 +0200

    Add transaction data mocking service

--- src/components/transactionPage/TransactionPage.js
+++ src/components/transactionPage/TransactionPage.js
@@ -1,7 +1,10 @@
 import React from "react";
 import FilterBar from "./FilterBar";
+import TransactionDataServiceMock from "../../mockingDataService/TransactionDataServiceMock";
 
 const TransactionPage = () => {
+  console.log(TransactionDataServiceMock());
+
   return (
     <div>
       <h1>Transakce</h1>
--- src/mockingDataService/TransactionDataServiceMock.js
+++ src/mockingDataService/TransactionDataServiceMock.js
@@ -1,9 +1,15 @@
 import faker from "faker";
+import * as moment from "moment";
 
 const getTransactionData = () => ({
-  date: faker.date.between("2018-01-01", "2019-01-05"),
-  label: faker.hacker.noun,
-  amount: faker.amount
+  date: moment(faker.date.between("2018-01-01", "2019-01-05")).format(
+    "YYYY-MM-DD HH:mm"
+  ),
+  label: faker.lorem.word(),
+  amount: faker.random.number({
+    min: -5000,
+    max: 5000
+  })
 });
 
-export default () => Array.from({ length: 20 });
+export default () => Array.from({ length: 20 }, getTransactionData);
